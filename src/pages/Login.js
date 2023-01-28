24ce9e1f0d793e44ce944d68a94d5988c41b1d9f
Gherson David <ing.gherson@yahoo.com>
2023-01-27 17:56:48 -0800

    test for auth

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import { Form, Input, Button, message } from "antd";
 import Cookies from "universal-cookie";
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
-import logo from '../images/favicon.ico'
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import logo from "../images/favicon.ico";
+import { Amplify, Auth } from "aws-amplify";
+import awsconfig from "./aws-exports";
+Amplify.configure(awsconfig);
 
 const cookies = new Cookies();
 const API_HOST = process.env.REACT_APP_API_HOST || "http://localhost:8000";
@@ -13,32 +16,30 @@ export const Login = () => {
   });
   const [loading, setLoading] = useState(false);
   //falta corregir para invocar el dynamo
-  const url = `${API_HOST}/api/login?email=${inputs.email}&password=${inputs.password}`;
+  //const url = `${API_HOST}/api/login?email=${inputs.email}&password=${inputs.password}`;
 
-  const getData = async () => {
-    setLoading(true);
-    const resp = await fetch(url, {
-      method: "POST",
-    });
-    setLoading(false);
-    const { data } = await resp.json();
-    if (data.nombre) {
-      cookies.set("nombre", data.nombre, {secure: true, sameSite: 'none'});
-      cookies.set("rfc", data.rfc, {secure: true, sameSite: 'none'});
-      cookies.set("address", data.direccion, {secure: true, sameSite: 'none'});
-      cookies.set("telefono", data.telefono, {secure: true, sameSite: 'none'});
-      cookies.set('email', inputs.email, {secure: true, sameSite: 'none'});
-      localStorage.setItem('token',data.token);
-      localStorage.setItem('nombre',data.nombre);
-      window.location.href = "./menu";
-    } else {
-      message.error("User or password incorrect");
+  const getData = async function signIn() {
+    try {
+      //aws amplify
+      const user = await Auth.signIn(username, password);
+      console.log(user);
+      setLoading(false);
+      // const { data } = await resp.json();
+      // if (data.nombre) {
+      //   cookies.set("nombre", data.nombre, { secure: true, sameSite: "none" });
+      //   // localStorage.setItem('token',data.token);
+      //   localStorage.setItem("nombre", data.nombre);
+      //   window.location.href = "./menu";
+      // } else {
+      //   message.error("User or password incorrect");
+      // }
+    } catch (error) {
+      console.log("error signing in", error);
     }
   };
 
   const onFinishFailed = () => {
-    message.error('Try again. Input email and password correctly.');
-
+    message.error("Try again. Input email and password correctly.");
   };
 
   const handleInputChange = (e) => {
@@ -63,67 +64,81 @@ export const Login = () => {
         </div>
         <div className="main">
           <div className="form-login">
-          <Form
-            name="PasswordMenu" //here defined info about form name
-            labelAlign="left"
-            layout="vertical"
-            initialValues={{
-              remember: true,
-            }}
-            onFinish={getData}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-          >
-            <Form.Item
-              label="Email"
-              name="email"
-              rules={[
-                {
-                  type: "email",
-                  message: "The email you entered isn’t connected to an account.",
-                },
-                {
-                  required: true,
-                  message: "Please input your username",
-                },
-              ]}
-            >
-              <Input
-                className="input_root"
-                name="email" prefix={<UserOutlined className="site-form-item-icon" />} onChange={handleInputChange} />
-            </Form.Item>
-
-            <Form.Item
-              label="Password"
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: "The password you’ve entered is incorrect.",
-                },
-              ]}
-            >
-              <Input.Password name="password" onChange={handleInputChange} prefix={<LockOutlined className="site-form-item-icon" />} />
-            </Form.Item>
-
-            <Form.Item
-              wrapperCol={{
-                offset: 8,
-                span: 16,
+            <Form
+              name="PasswordMenu" //here defined info about form name
+              labelAlign="left"
+              layout="vertical"
+              initialValues={{
+                remember: true,
               }}
+              onFinish={getData}
+              onFinishFailed={onFinishFailed}
+              autoComplete="off"
             >
-              <Button
-                className="boton"
-                type="primary" htmlType="submit" loading={loading}>
-                Log In
-              </Button>
-              <Button
-                className="boton"
-                type="secondary" htmlType="submit" loading={loading}>
-                Sign Up
-              </Button>
-            </Form.Item>
-          </Form>
+              <Form.Item
+                label="Email"
+                name="email"
+                rules={[
+                  {
+                    type: "email",
+                    message:
+                      "The email you entered isn’t connected to an account.",
+                  },
+                  {
+                    required: true,
+                    message: "Please input your username",
+                  },
+                ]}
+              >
+                <Input
+                  className="input_root"
+                  name="email"
+                  prefix={<UserOutlined className="site-form-item-icon" />}
+                  onChange={handleInputChange}
+                />
+              </Form.Item>
+
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "The password you’ve entered is incorrect.",
+                  },
+                ]}
+              >
+                <Input.Password
+                  name="password"
+                  onChange={handleInputChange}
+                  prefix={<LockOutlined className="site-form-item-icon" />}
+                />
+              </Form.Item>
+
+              <Form.Item
+                wrapperCol={{
+                  offset: 8,
+                  span: 16,
+                }}
+              >
+                <Button
+                  className="boton"
+                  type="primary"
+                  htmlType="submit"
+                  loading={loading}
+                >
+                  Log In
+                </Button>
+                <Button
+                  className="boton"
+                  type="secondary"
+                  htmlType="submit"
+                  loading={loading}
+                >
+                  Sign Up
+                </Button>
+              </Form.Item>
+            </Form>
           </div>
         </div>
         <div className="footer">
@@ -133,10 +148,8 @@ export const Login = () => {
       {/* imagen subir  */}
       <div className="total-login-images">
         <input type="file" className="total-login-images" />
-          <button >
-            Upload!
-          </button>
+        <button>Upload!</button>
       </div>
     </div>
-  )
+  );
 };
